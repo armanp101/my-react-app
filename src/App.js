f5c15bc0c03857ecc69aaf7aaac52218b036f508
armanp101 <a.b.patel@wustl.edu>
2025-04-03 15:34:44 -0500

    Ensured square is empty prior to X or O placement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({value, onSquareClick}) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -8,24 +8,22 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function handleClick(i) {
-  const nextSquares = squares.slice();
-  if (xIsNext) {
-    nextSquares[i] = "X";
-  } else {
-    nextSquares[i] = "O";
-  }
-  setSquares(nextSquares);
-  setXIsNext(!xIsNext);
-}
-
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
