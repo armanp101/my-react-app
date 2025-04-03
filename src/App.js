bd476a804c3d7d346c6aa12fee4e9611a09ba2a4
armanp101 <a.b.patel@wustl.edu>
2025-04-03 15:30:25 -0500

    Implemented handleClick () to identify user turn and swich off

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function handleClick(i) {
+  const nextSquares = squares.slice();
+  if (xIsNext) {
+    nextSquares[i] = "X";
+  } else {
+    nextSquares[i] = "O";
+  }
+  setSquares(nextSquares);
+  setXIsNext(!xIsNext);
+}
+
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
