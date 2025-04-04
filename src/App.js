1c6fa83af24a4cc081ff51a3b25910cdb093f24a
armanp101 <a.b.patel@wustl.edu>
2025-04-03 19:02:11 -0500

    Added player tracking and the history of moves:

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function Square({value, onSquareClick}) {
 }
 
 export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
   return (
     <div className="game">
       <div className="game-board">
