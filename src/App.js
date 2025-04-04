dfb1cd0549aa33cc4593dc6327a52f9c45b60e9c
armanp101 <a.b.patel@wustl.edu>
2025-04-03 19:10:55 -0500

    Finalized board functio nto accruatly track and display turns and automatically check for winner on each click

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
+function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -28,19 +28,17 @@ function handlePlay(nextSquares) {
 }
 
 function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {}
-
-  function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
-
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
