bfcb8fd10465485ade7d1e83188efc98fe7323b6
armanp101 <a.b.patel@wustl.edu>
2025-04-03 19:12:11 -0500

    Fixed Game function to work with new history tracking feature

--- src/App.js
+++ src/App.js
@@ -8,24 +8,6 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Game(){
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <ol>{}</ol>
-      </div>
-    </div>
-  );
-}
-
-function handlePlay(nextSquares) {
-}
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
@@ -70,6 +52,16 @@ function Board({ xIsNext, squares, onPlay }) {
     </>
   );
 }
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
 
 function calculateWinner(squares) {
   const lines = [
